73fd3c35d5fd1dabfb70152b1612f9d56a737c5f
Jacob Day <jacoblovesmath@gmail.com>
2025-05-23 12:52:15 -0400

    Added more to game state and tick update. Player draw still not working.

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,8 @@ const Vec2 = rl.Vector2;
 const SCREEN_SIZE = 700;
 const CELL_SIZE = 50;
 const GRID_WIDTH = SCREEN_SIZE / CELL_SIZE;
+const TICK_SPEED = 5;
+const MAX_SNAKE_LEN = 256;
 
 const Direction = enum {
     UP,
@@ -15,7 +17,7 @@ const Direction = enum {
 };
 
 const Snake = struct {
-    position: [GRID_WIDTH * GRID_WIDTH]Vec2,
+    position: [MAX_SNAKE_LEN]Vec2,
     direction: Direction,
     length: u8,
 
@@ -31,21 +33,73 @@ const Game = struct {
     player: *Snake,
     item: Vec2,
     score: u32,
+    moveAllowed: bool,
+    prevPos: [MAX_SNAKE_LEN]Vec2,
+    frameCounter: u64,
 
     pub fn drawFood(game: Game) void {
-        rl.drawCircle(@intFromFloat(@as(f32, CELL_SIZE) * (game.item.x + 1) / 2), @intFromFloat(@as(f32, CELL_SIZE) * (game.item.y + 1) / 2), @floatFromInt(CELL_SIZE / 2), .red);
+        // 0 -> 25; 1 -> 75; etc.
+        // f(x) = 25 + 50x
+        rl.drawCircle(@intFromFloat(CELL_SIZE / 2 + CELL_SIZE * game.item.x), @intFromFloat(CELL_SIZE / 2 + CELL_SIZE * game.item.y), CELL_SIZE / 2, .red);
+    }
+
+    pub fn tick(game: *Game) void {
+        if (game.*.isRunning) {
+            var pl = game.*.player.*;
+
+            // poll for direction
+            if (rl.isKeyPressed(.right) and game.*.moveAllowed) {
+                pl.direction = Direction.RIGHT;
+                game.*.moveAllowed = false;
+            } else if (rl.isKeyPressed(.left) and game.*.moveAllowed) {
+                pl.direction = Direction.LEFT;
+                game.*.moveAllowed = false;
+            } else if (rl.isKeyPressed(.down) and game.*.moveAllowed) {
+                pl.direction = Direction.DOWN;
+                game.*.moveAllowed = false;
+            } else if (rl.isKeyPressed(.up) and game.*.moveAllowed) {
+                pl.direction = Direction.UP;
+                game.*.moveAllowed = false;
+            }
+
+            game.*.prevPos = pl.position;
+
+            if (game.*.frameCounter % TICK_SPEED == 0) {
+                switch (pl.direction) {
+                    .DOWN => {
+                        pl.position[0].y -= CELL_SIZE;
+                    },
+                    .UP => {
+                        pl.position[0].y += CELL_SIZE;
+                    },
+                    .LEFT => {
+                        pl.position[0].x -= CELL_SIZE;
+                    },
+                    .RIGHT => {
+                        pl.position[0].y += CELL_SIZE;
+                    },
+                }
+                game.*.moveAllowed = true;
+
+                for (1..pl.length) |i| {
+                    pl.position[i] = game.*.prevPos[i - 1];
+                }
+            }
+
+            game.*.frameCounter += 1;
+        }
     }
 };
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    _ = std.io.getStdOut().writer(); // for stdout
 
     rl.initWindow(SCREEN_SIZE, SCREEN_SIZE, "znake");
     defer rl.closeWindow();
 
     var player = Snake{
         .position = undefined,
-        .direction = Direction.RIGHT,
+        .direction = Direction.DOWN,
         .length = 3,
     };
 
@@ -53,17 +107,23 @@ pub fn main() !void {
         player.position[i] = Vec2.init(0, 0);
     }
 
-    const game = Game{
+    var game = Game{
         .isRunning = true,
         .item = Vec2.init(@floatFromInt(rl.getRandomValue(0, GRID_WIDTH)), @floatFromInt(rl.getRandomValue(0, GRID_WIDTH))),
         .score = 0,
         .player = &player,
+        .moveAllowed = false,
+        .prevPos = undefined,
+        .frameCounter = 0,
     };
 
-    try stdout.print("Food drawn at cell {d},{d}\n", .{ game.item.x, game.item.y });
-    try stdout.print("Actual grid position: {d}, {d}\n", .{ @as(f32, CELL_SIZE) * (game.item.x + 1) / 2, @as(f32, CELL_SIZE) * (game.item.y + 1) / 2 });
+    rl.setTargetFPS(60);
 
     while (!rl.windowShouldClose()) {
+        // update game
+        game.tick();
+
+        // draw game
         rl.beginDrawing();
         defer rl.endDrawing();
 
@@ -76,7 +136,7 @@ pub fn main() !void {
             i += CELL_SIZE;
         }
 
-        game.player.drawPlayer();
+        game.player.*.drawPlayer();
         game.drawFood();
     }
 }

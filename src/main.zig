2321cfefc4dea59a7ae0cce719f0c366ecf9d655
Jacob Day <jacoblovesmath@gmail.com>
2025-05-23 14:18:40 -0400

    Snake now moves! yayyyy

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ const CELL_SIZE = 50;
 const GRID_WIDTH = SCREEN_SIZE / CELL_SIZE;
 const TICK_SPEED = 5;
 const MAX_SNAKE_LEN = 256;
+const POS_HEAD = 0;
 
 const Direction = enum {
     UP,
@@ -43,57 +44,76 @@ const Game = struct {
         rl.drawCircle(@intFromFloat(CELL_SIZE / 2 + CELL_SIZE * game.item.x), @intFromFloat(CELL_SIZE / 2 + CELL_SIZE * game.item.y), CELL_SIZE / 2, .red);
     }
 
-    pub fn tick(game: *Game) void {
+    pub fn tick(game: *Game) !void {
+        _ = std.io.getStdOut().writer();
+
         if (game.*.isRunning) {
-            var pl = game.*.player.*;
+            const pl = game.*.player;
 
             // poll for direction
             if (rl.isKeyPressed(.right) and game.*.moveAllowed) {
-                pl.direction = Direction.RIGHT;
+                pl.*.direction = Direction.RIGHT;
                 game.*.moveAllowed = false;
             } else if (rl.isKeyPressed(.left) and game.*.moveAllowed) {
-                pl.direction = Direction.LEFT;
+                pl.*.direction = Direction.LEFT;
                 game.*.moveAllowed = false;
             } else if (rl.isKeyPressed(.down) and game.*.moveAllowed) {
-                pl.direction = Direction.DOWN;
+                pl.*.direction = Direction.DOWN;
                 game.*.moveAllowed = false;
             } else if (rl.isKeyPressed(.up) and game.*.moveAllowed) {
-                pl.direction = Direction.UP;
+                pl.*.direction = Direction.UP;
                 game.*.moveAllowed = false;
             }
 
-            game.*.prevPos = pl.position;
+            for (0..pl.*.length) |i| {
+                game.*.prevPos[i] = pl.*.position[i];
+            }
 
             if (game.*.frameCounter % TICK_SPEED == 0) {
-                switch (pl.direction) {
+                switch (pl.*.direction) {
                     .DOWN => {
-                        pl.position[0].y -= CELL_SIZE;
+                        pl.*.position[POS_HEAD].y += CELL_SIZE;
                     },
                     .UP => {
-                        pl.position[0].y += CELL_SIZE;
+                        pl.*.position[POS_HEAD].y -= CELL_SIZE;
                     },
                     .LEFT => {
-                        pl.position[0].x -= CELL_SIZE;
+                        pl.*.position[POS_HEAD].x -= CELL_SIZE;
                     },
                     .RIGHT => {
-                        pl.position[0].y += CELL_SIZE;
+                        pl.*.position[POS_HEAD].x += CELL_SIZE;
                     },
                 }
                 game.*.moveAllowed = true;
 
-                for (1..pl.length) |i| {
-                    pl.position[i] = game.*.prevPos[i - 1];
+                for (1..pl.*.length) |i| {
+                    pl.*.position[i] = game.*.prevPos[i - 1];
                 }
             }
 
             game.*.frameCounter += 1;
         }
     }
+
+    pub fn render(game: Game) void {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.ray_white);
+
+        var i: i32 = 0;
+        while (i <= SCREEN_SIZE) {
+            rl.drawLine(i, 0, i, SCREEN_SIZE, .dark_gray);
+            rl.drawLine(0, i, SCREEN_SIZE, i, .dark_gray);
+            i += CELL_SIZE;
+        }
+
+        game.drawFood();
+        game.player.*.drawPlayer();
+    }
 };
 
 pub fn main() !void {
-    _ = std.io.getStdOut().writer(); // for stdout
-
     rl.initWindow(SCREEN_SIZE, SCREEN_SIZE, "znake");
     defer rl.closeWindow();
 
@@ -109,7 +129,7 @@ pub fn main() !void {
 
     var game = Game{
         .isRunning = true,
-        .item = Vec2.init(@floatFromInt(rl.getRandomValue(0, GRID_WIDTH)), @floatFromInt(rl.getRandomValue(0, GRID_WIDTH))),
+        .item = Vec2.init(@floatFromInt(rl.getRandomValue(1, GRID_WIDTH - 1)), @floatFromInt(rl.getRandomValue(1, GRID_WIDTH - 1))),
         .score = 0,
         .player = &player,
         .moveAllowed = false,
@@ -121,22 +141,9 @@ pub fn main() !void {
 
     while (!rl.windowShouldClose()) {
         // update game
-        game.tick();
+        try game.tick();
 
         // draw game
-        rl.beginDrawing();
-        defer rl.endDrawing();
-
-        rl.clearBackground(.ray_white);
-
-        var i: i32 = 0;
-        while (i <= SCREEN_SIZE) {
-            rl.drawLine(i, 0, i, SCREEN_SIZE, .dark_gray);
-            rl.drawLine(0, i, SCREEN_SIZE, i, .dark_gray);
-            i += CELL_SIZE;
-        }
-
-        game.player.*.drawPlayer();
-        game.drawFood();
+        game.render();
     }
 }
